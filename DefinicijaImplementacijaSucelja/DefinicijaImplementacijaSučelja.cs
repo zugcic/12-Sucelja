898eadbdaa6e831a68b66e7ae38eb1386432236a
Julijan Šribar <julijan.sribar@gmail.com>
2016-12-18 00:09:12 +0100

    Dorađeno za 2016.

---
+++ DefinicijaImplementacijaSucelja/DefinicijaImplementacijaSučelja.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Vsite.CSharp
+{
+    // definicija sučelja:
+    public interface IMojeSučelje
+    {
+        // deklaracija (samo potpis) metode:
+        void GenerirajMojDogađaj();
+        // deklaracija svojstva:
+        string Svojstvo { set; get; }
+        // deklaracija dogadjaja:
+        event MojDelegat MojDogađaj;
+    }
+
+    // deklaracija delegata
+    public delegate void MojDelegat(object sender, EventArgs e);
+
+
+    // TODO: Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
+    // TODO: Implementirajte metodu GenerirajMojDogađaj tako da ona generira događaj MojDogađaj
+    // TODO: Implementirajte kod za Svojstvo
+    public class Implementacija
+    {
+    }
+
+    public class DefinicijaImplementacijaSučelja
+    {
+        // metode kao argument ne primaju konkretne tipove nego sučelja! 
+        static void IspišiSvojstvo(IMojeSučelje ms)
+        {
+            ms.Svojstvo = "Dobar dan";
+            Console.WriteLine(ms.Svojstvo);
+        }
+
+        static void DigniDogađaj(IMojeSučelje ms)
+        {
+            ms.MojDogađaj += ObradiDogađaj;
+            ms.GenerirajMojDogađaj();
+        }
+
+        static void ObradiDogađaj(object sender, EventArgs args)
+        {
+            Console.WriteLine("Događaj obrađen!");
+        }
+
+        static void Main(string[] args)
+        {
+            // TODO: Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
+            IMojeSučelje ms = null;
+
+            IspišiSvojstvo(ms);
+            DigniDogađaj(ms);
+
+            Console.WriteLine("GOTOVO!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Testovi/TestDefinicijaImplementacijaSučelja.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.CSharp;
+
+namespace Vsite.CSharp.Testovi
+{
+    [TestClass]
+    public class TestDefinicijaImplementacijaSučelja
+    {
+        [TestMethod]
+        public void DefinicijaImplementacijaSučelja_SvojstvoVraćaZadanuVrijednost()
+        {
+            IMojeSučelje i = (IMojeSučelje)(new Implementacija());
+            i.Svojstvo = "abcd";
+            Assert.AreEqual("abcd", i.Svojstvo);
+        }
+
+        [TestMethod]
+        public void DefinicijaImplementacijaSučelja_PozivMetodeGeneriraDogađaj()
+        {
+            IMojeSučelje i = (IMojeSučelje)(new Implementacija());
+            događajJeObrađen = false;
+            i.MojDogađaj += ObradaDogađaja;
+            i.GenerirajMojDogađaj();
+            Assert.AreEqual(true, događajJeObrađen);
+        }
+
+        void ObradaDogađaja(object sender, EventArgs args)
+        {
+            događajJeObrađen = true;
+        }
+
+        private bool događajJeObrađen = false;
+    }
+}
