c99724136170742dfc5648f12937ce730ea42b3e
Julijan Šribar <julijan.sribar@gmail.com>
2016-12-18 00:36:18 +0100

    Popravljeno za 2016.

---
+++ Testovi/TestVirtualneMetode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.CSharp;
+
+namespace Vsite.CSharp.Testovi
+{
+    [TestClass]
+    public class TestVirtualneMetode
+    {
+        [TestMethod]
+        public void VirtualniČlanovi_BaznaVirtualnaMetodaVraćaTekst()
+        {
+            VirtualniČlanovi.ISučelje s = (VirtualniČlanovi.ISučelje)(new VirtualniČlanovi.Bazna());
+            Assert.AreEqual("Bazna.VirtualnaMetoda", s.VirtualnaMetoda());
+        }
+
+        [TestMethod]
+        public void VirtualniČlanovi_BaznaNevirtualnaMetodaVraćaTekst()
+        {
+            VirtualniČlanovi.ISučelje s = (VirtualniČlanovi.ISučelje)(new VirtualniČlanovi.Bazna());
+            Assert.AreEqual("Bazna.NevirtualnaMetoda", s.NevirtualnaMetoda());
+        }
+
+        [TestMethod]
+        public void VirtualniČlanovi_IzvedenaVirtualnaMetodaVraćaTekst()
+        {
+            VirtualniČlanovi.ISučelje s = (VirtualniČlanovi.ISučelje)(new VirtualniČlanovi.Izvedena());
+            Assert.AreEqual("Izvedena.VirtualnaMetoda", s.VirtualnaMetoda());
+        }
+
+        [TestMethod]
+        public void VirtualniČlanovi_IzvedenaNevirtualnaMetodaVraćaTekst()
+        {
+            VirtualniČlanovi.ISučelje s = (VirtualniČlanovi.ISučelje)(new VirtualniČlanovi.Izvedena());
+            Assert.AreEqual("Bazna.NevirtualnaMetoda", s.NevirtualnaMetoda());
+        }
+    }
+}
---
+++ VirtualniClanovi/VirtualniČlanovi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Vsite.CSharp
+{
+    public class VirtualniČlanovi
+    {
+        public interface ISučelje
+        {
+            string NevirtualnaMetoda();
+            string VirtualnaMetoda();
+        }
+
+        // TODO: Definirajte klasu Bazna tako da implementira sučelje ISučelje. Metoda NevirtualnaMetoda neka vraća "Bazna.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Bazna.VirtualnaMetoda".
+        public class Bazna
+        {
+        }
+
+
+        // TODO: Definirajte klasu Izvedena da je izvedena iz klase Bazna te u klasi Izvedena pregazite (override) metode iz ISučelja. Metoda NevirtualnaMetoda neka vraća "Izvedena.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Izvedena.VirtualnaMetoda".
+        // TODO: Provjerite poruke o pogreškama ili upozorenja prevoditelja.
+        public class Izvedena
+        {
+        }
+
+        public static void PozoviNevirtualnuMetodu(ISučelje sučelje)
+        {
+            Console.WriteLine(sučelje.NevirtualnaMetoda());
+        }
+
+        public static void PozoviVirtualnuMetodu(ISučelje sučelje)
+        {
+            Console.WriteLine(sučelje.VirtualnaMetoda());
+        }
+
+        static void Main(string[] args)
+        {
+            // TODO: Inicijalizirajte objekt instancom klase Bazna te provjerite ispis
+            ISučelje s1 = null;
+            PozoviNevirtualnuMetodu(s1);
+            PozoviVirtualnuMetodu(s1);
+
+            // TODO: Inicijalizirajte objekt instancom klase Izvedena te provjerite ispis
+            ISučelje s2 = null;
+            PozoviNevirtualnuMetodu(s2);
+            PozoviVirtualnuMetodu(s2);
+
+            Console.WriteLine("GOTOVO!!!");
+            Console.ReadKey();
+        }
+    }
+}
