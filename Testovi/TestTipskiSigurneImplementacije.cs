4e14dd5ede7ed9985be67ac6c435ac7945d674a1
Julijan Šribar <julijan.sribar@gmail.com>
2016-12-18 13:16:52 +0100

    Popravljen projekt TipskiSigurneImplementacije te dodan test.

---
+++ Testovi/TestTipskiSigurneImplementacije.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.CSharp.Testovi
+{
+    [TestClass]
+    public class TestTipskiSigurneImplementacije : ConsoleTest
+    {
+        [TestMethod]
+        public void TipskiSigurneImplementacije_UsporedbaDvijuDuljinaVraćaVećeOdNuleAkoJePrvaDuljinaVeća()
+        {
+            TipskiSigurneImplementacije.Duljina d1 = new TipskiSigurneImplementacije.Duljina(5);
+            TipskiSigurneImplementacije.Duljina d2 = new TipskiSigurneImplementacije.Duljina(3);
+            TipskiSigurneImplementacije.Usporedi(d1, d2);
+            Assert.IsTrue(cw.GetString().Contains(">"));
+        }
+
+        [TestMethod]
+        public void TipskiSigurneImplementacije_UsporedbaDvijuDuljinaVraćaVećeManjeOdNuleAkoJePrvaDuljinaManja()
+        {
+            TipskiSigurneImplementacije.Duljina d1 = new TipskiSigurneImplementacije.Duljina(3);
+            TipskiSigurneImplementacije.Duljina d2 = new TipskiSigurneImplementacije.Duljina(5);
+            TipskiSigurneImplementacije.Usporedi(d1, d2);
+            Assert.IsTrue(cw.GetString().Contains("<"));
+        }
+
+        [TestMethod]
+        public void TipskiSigurneImplementacije_UsporedbaDvijuDuljinaVraćaNuluAkoJePrvaDuljinaJednakaDrugoj()
+        {
+            TipskiSigurneImplementacije.Duljina d1 = new TipskiSigurneImplementacije.Duljina(3);
+            TipskiSigurneImplementacije.Duljina d2 = new TipskiSigurneImplementacije.Duljina(3);
+            TipskiSigurneImplementacije.Usporedi(d1, d2);
+            Assert.IsTrue(cw.GetString().Contains("="));
+        }
+    }
+}
--- TipskiSigurneImplementacije/TipskiSigurneImplementacije.cs
+++ TipskiSigurneImplementacije/TipskiSigurneImplementacije.cs
@@ -2,37 +2,56 @@
 
 namespace Vsite.CSharp
 {
-    class Program
+    public class TipskiSigurneImplementacije
     {
+        public struct Duljina
+        {
+            private int duljina;
+
+            public Duljina(int duljina)
+            {
+                this.duljina = duljina;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0} m", duljina);
+            }
+        }
+
+        public static void Usporedi(Duljina d1, object d2)
+        {
+            int usporedba = 0; // d1.CompareTo(d2);
+            if (usporedba == 0)
+                Console.WriteLine("{0} = {1}", d1, d2);
+            else if (usporedba > 0)
+                Console.WriteLine("{0} > {1}", d1, d2);
+            else
+                Console.WriteLine("{0} < {1}", d1, d2);
+        }
+
         static void Main(string[] args)
         {
             try
             {
+                // TODO: Dopuniti strukturu Duljina tako da implementira sučelje IComparable i napisati kod za metodu CompareTo.
+
+                // TODO: U metodi Usporedi zamijeniti kraj prve naredbe zakomentiranim pozivom. 
+                // TODO: U metodi Main dodati naredbu koja će metodi Usporedi proslijediti duljine d1 i d2 te provjeriti ispis.
                 Duljina d1 = new Duljina(2);
                 Duljina d2 = new Duljina(1);
 
-                // TODO: Dopuniti strukturu Duljina tako da implementira sučelje IComparable i napisati kod za metodu CompareTo
-
-
-                // TODO: Dodati naredbu koja će usporediti duljine d1 i d2 te ispisati koja je veća
-
-
-                // TODO: Dodati naredbu koja će usporediti d1 sa stringom "pero". Provjeriti da li prevoditelj javlja pogrešku te ako nema pogreške, pokrenut program.
-
+                // TODO: Dodati naredbu koja će metodi Usporedi proslijediti d1 i string "pero". Provjeriti javlja li prevoditelj pogrešku te ako nema pogreške, pokrenuti program i provjeriti ispis.
 
                 // TODO: Promijeniti implementaciju sučelja IComparable tako da prevoditelj javi pogrešku na usporedbu strukture Duljina i stringa. Provjeriti javlja li prevoditelj pogrešku.
 
-
                 // TODO: Dopuniti strukturu Duljina tako da implementira i sučelje IComparable<Duljina>. Provjeriti javlja li prevoditelj pogrešku.
-
-
 
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
